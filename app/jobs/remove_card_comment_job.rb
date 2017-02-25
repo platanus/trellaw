3114ccd794db2bd5da3ae5d610c33214e4dfb566
Ignacio Baixas <iobaixas@gmail.com>
2017-02-25 18:16:58 -0300

    feat(): implements RemoveCardCommentJob

--- app/jobs/remove_card_comment_job.rb
+++ app/jobs/remove_card_comment_job.rb
@@ -1,7 +1,14 @@
 class RemoveCardCommentJob < ActiveJob::Base
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(_violation)
+    trello_client.edit_card_comment(_violation.comment_tid, '...')
+    _violation.update_column(:comment_tid, nil)
+  end
+
+  private
+
+  def trello_client
+    @trello_client ||= TrelloUtils.summon_the_monkey
   end
 end
--- spec/jobs/remove_card_comment_job_spec.rb
+++ spec/jobs/remove_card_comment_job_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe RemoveCardCommentJob, type: :job do
-  pending "implement RemoveCardCommentJob"
+  let(:client) { mock_ham_trello_client }
+  let(:violation) { create(:violation, comment_tid: 'tid') }
+
+  it "calls edit_card_comment and then resets the violation comment_tid" do
+    expect(client).to receive(:edit_card_comment).with(violation.comment_tid, instance_of(String))
+      .and_return build(:trello_comment)
+
+    expect { RemoveCardCommentJob.perform_now(violation) }
+      .to change { violation.reload.comment_tid }.to(nil)
+  end
 end

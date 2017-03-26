1de412fc22975297c6fdcefd531073d9a1658762
Juan Ignacio Donoso <jidonoso@gmail.com>
2017-03-26 18:43:38 -0400

    wip

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,21 +1,21 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
-config_file = "#{Rails.root}/config/redis.yml"
-redis_config = YAML.load(ERB.new(File.read(config_file)).result)[Rails.env]
+# config_file = "#{Rails.root}/config/redis.yml"
+# redis_config = YAML.load(ERB.new(File.read(config_file)).result)[Rails.env]
 
-redis_conn = proc do
-  Redis.new(redis_config)
-end
-Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: 5, &redis_conn)
-end
-Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: 27, &redis_conn)
-  config.server_middleware do |chain|
-    chain.remove Sidekiq::Middleware::Server::RetryJobs
-  end
-end
+# redis_conn = proc do
+#   Redis.new(redis_config)
+# end
+# Sidekiq.configure_client do |config|
+#   config.redis = ConnectionPool.new(size: 2, &redis_conn)
+# end
+# Sidekiq.configure_server do |config|
+#   config.redis = ConnectionPool.new(size: 16, &redis_conn)
+#   config.server_middleware do |chain|
+#     chain.remove Sidekiq::Middleware::Server::RetryJobs
+#   end
+# end
 
 if !Rails.env.development?
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|

8fcb66a33428b4a361e9cb488ed49fc2043b2b0f
Leandro Segovia <ldlsegovia@gmail.com>
2017-09-15 10:33:31 -0300

    feat(Board): add methods to access Trello client from Board model transparently

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -18,4 +18,34 @@ RSpec.describe Board, type: :model do
     it { is_expected.to validate_presence_of(:board_tid) }
     it { is_expected.to validate_uniqueness_of(:board_tid) }
   end
+
+  describe '#trello_lists' do
+    let(:trello_client) { double(get_lists: "list") }
+
+    before do
+      expect(TrelloUtils).to receive(:load_user_client).with(subject.user).and_return(trello_client)
+    end
+
+    it { expect(subject.trello_lists).to eq("list") }
+  end
+
+  describe 'trello board attributes' do
+    let(:get_board_result) do
+      double(
+        tid: 'X',
+        name: 'Lean',
+        description: 'desc'
+      )
+    end
+
+    let(:trello_client) { double(get_board: get_board_result) }
+
+    before do
+      expect(TrelloUtils).to receive(:load_user_client).with(subject.user).and_return(trello_client)
+    end
+
+    it { expect(subject.tid).to eq('X') }
+    it { expect(subject.name).to eq('Lean') }
+    it { expect(subject.description).to eq('desc') }
+  end
 end

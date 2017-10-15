d1ae7ebf6448a07e5a57c55356038f836793f83d
Leandro Segovia <ldlsegovia@gmail.com>
2017-10-15 01:39:05 -0300

    feat(): add CardViolation class

---
+++ spec/factories/law_violation.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :law_violation do
+    name "foo"
+    law_name "member_limit"
+    condition_proc { Proc.new { true } }
+    comment_proc { Proc.new { "comment" } }
+
+    skip_create
+    initialize_with { new(attributes) }
+
+    factory :card_violation, class: LawViolations::CardViolation do
+      # just use defaults
+    end
+  end
+end

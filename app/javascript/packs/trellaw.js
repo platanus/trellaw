5ba79b875f9abd79d80821494d34ac57e42ebc62
Guillermo Moreno <gmq@guillermomoreno.cl>
2017-11-10 15:46:15 -0300

    chore(): clean structure

--- app/javascript/packs/trellaw.js
+++ app/javascript/packs/trellaw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import Vue from 'vue';
-import App from './app.vue';
+import App from '../trellaw/app.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue(App).$mount('#app');
